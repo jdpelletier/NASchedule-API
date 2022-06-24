c0d2a83cb0fbc61ec05f0793e17c7948eab7c930
jdpelletier <john.pelletier@ymail.com>
2022-06-24 11:16:58 -1000

    updates to run on keck server (testing)

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 # from flask_sqlalchemy import SQLAlchemy
 
 # db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    cors = CORS(app)
+    # cors = CORS(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
 
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -18,3 +18,15 @@ def create_app():
     app.register_blueprint(views.main)
 
     return app
+
+
+if __name__ == '__main__':
+
+    app = create_app()
+    host = '0.0.0.0'
+    port = 53872
+
+    # run flask server
+    print(f"Starting NA Homepage API: PORT = {port})
+    app.run(host=host, port=port)
+    print("Stopping NA Homepage API.")
